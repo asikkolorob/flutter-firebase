eb4720591173c0ad2b51005b11f901d21481539c
asikkolorob <asikkolorob@gmail.com>
2022-02-03 08:33:20 +0600

    project setup

---
+++ flutter_firbase/lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const cMobileBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
+const cWebBackgroundColor = Color.fromRGBO(18, 18, 18, 1);
+const cMobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
+const cBlueColor = Color.fromRGBO(0, 149, 246, 1);
+const cPrimaryColor = Colors.white;
+const cSecondaryColor = Colors.grey;
+
